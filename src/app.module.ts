f4583329cba8e8d82d81278c2858b8e0c47b4f58
Or Arbel <orarbel@appricots.onmicrosoft.com>
2025-09-09 14:28:47 +0300

    add status module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlertsModule } from './alerts/alerts.module';
+import { StatusModule } from './status/status.module';
 
 @Module({
-  imports: [AlertsModule],
+  imports: [AlertsModule, StatusModule],
   controllers: [AppController],
   providers: [AppService],
 })
