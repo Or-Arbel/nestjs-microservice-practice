fb72fb2a84c239d6fdc18ef0a7a443395c2d050b
Or Arbel <orarbel@appricots.onmicrosoft.com>
2025-09-09 14:04:58 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AlertsModule } from './alerts/alerts.module';
+
+@Module({
+  imports: [AlertsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
