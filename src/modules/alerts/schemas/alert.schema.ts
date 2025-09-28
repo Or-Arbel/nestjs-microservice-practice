29627e7225350e3ad2bdb719a401be40e846919a
Or Arbel <orarbel@appricots.onmicrosoft.com>
2025-09-28 13:01:58 +0300

    add alert schema in src/modules/alerts/schemas

---
+++ src/modules/alerts/schemas/alert.schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const CreateAlertSchema = z.object({
+  name: z.string().min(1).max(100),
+  create_date: z.iso.datetime(),
+  location: z.object({
+    // check later and fix if needed
+    lat: z.number().min(29).max(34),
+    lng: z.number().min(34).max(36),
+  }),
+  alerts: z.array(z.number().int().nonnegative()).nonempty(),
+  status: z.number().min(1).max(6),
+  description: z.string().min(1).max(500),
+});
+
+export const UpdateAlertSchema = CreateAlertSchema.extend({
+  id: z.string(), // needed in order to  update a record
+});
